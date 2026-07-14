12868635f5fe3372b2f58c74e59e15a9b3695a92
ZHU <a0903223681@gmail.com>
2026-07-14 21:37:24 +0800

    output xml and txt

--- project-v1_0714.py
+++ project-v1_0714.py
@@ -1,83 +1,67 @@
 #!/usr/bin/env python3
 
 import argparse
-
 import ipaddress
-
 import subprocess
 
 from pathlib import Path
-
 from datetime import datetime
 
 OUTPUT_DIR = Path("output")
-
 OUTPUT_DIR.mkdir(exist_ok=True)
 
 def validate_ip(ip: str) -> str:
-
     return str(ipaddress.ip_address(ip))
 
-def run_nmap(ip: str) -> tuple[int, str]:
-
+def run_nmap(ip: str, base_name) -> tuple[int, str, str]:
+    xml_path = OUTPUT_DIR / f"{base_name}.xml"
+    txt_path = OUTPUT_DIR / f"{base_name}.txt"
+    
     cmd = [
-
         "nmap",
-
         "-sV",
-
         "-Pn",
-
         "-T3",
-
         "--open",
-
+        "-oX", str(xml_path),
+        "-oN", str(txt_path),
         ip,
-
     ]
 
     result = subprocess.run(
-
         cmd,
-
         capture_output=True,
-
         text=True,
-
         check=False
-
     )
 
-    return result.returncode, result.stdout + "\n" + result.stderr
-
-def save_result(ip: str, content: str) -> Path:
-
-    ts = datetime.now().strftime("%Y%m%d_%H%M%S")
-
-    filename = OUTPUT_DIR / f"nmap_{ip}_{ts}.txt"
-
-    filename.write_text(content, encoding="utf-8")
-
-    return filename
+    return result.returncode, str(xml_path), str(txt_path), result.stderr
 
 def main():
 
     parser = argparse.ArgumentParser(description="Simple IoT initial scanner")
-
     parser.add_argument("ip", help="Target IP address")
-
     args = parser.parse_args()
 
     ip = validate_ip(args.ip)
+    time = datetime.now().strftime("%Y%m%d_%H%M%S")
+    base_name = f"nmap_{ip}_{time}"
 
-    code, output = run_nmap(ip)
-
-    saved_file = save_result(ip, output)
+    code, xml_file, txt_file, err_msg = run_nmap(ip, base_name)
 
     print(f"Scan finished. Return code: {code}")
 
-    print(f"Saved to: {saved_file}")
+    if Path(xml_file).exists():
+        print(f"XML saved to: {xml_file}")
+    else:
+        print(f"XML NOT created (expected at: {xml_file})")
+
+    if Path(txt_file).exists():
+        print(f"TXT saved to: {txt_file}")
+    else:
+        print(f"TXT NOT created (expected at: {txt_file})")
+
+    # print(f"Saved to: {saved_file}")
 
 if __name__ == "__main__":
-
     main()
